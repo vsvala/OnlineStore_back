812e06a67425805be05b41d5c29aad9cb4562e84
Virva <virva.svala@gmail.com>
2021-12-02 21:53:25 +0200

    working versio of  get products function

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 const express = require('express')
-const app = express()
+const axios = require('axios')
 const cors = require ('cors')
+const app = express()
 
-app.use(express.static('public'))
 app.use(cors())
 
+app.use(express.static('public'))
 
-app.get('api/products', async (req,res)=> {
+app.use(express.json())
 
- try{
- //ngnix kun käyttää mitään ei tapahdu...    
-//const response = await axios.get('http//nginx:9000/api/v1/products') 
-const response = await axios.get('http//localhost:nginx/api/v1/products')
-
-console.log("responceeee from back ", response.data)
-const res=response.data
-return res
-
-} catch (error) {
-    return { error: 'uups! Could not get products from server' }
-    }
-
+app.get('/api/products', async (req, res) => {
+    console.log('Requesting products from nginx...')
+    const response = await axios.get('http://nginx:9000/api/v1/products')
+    console.log("Responceeee from back ", response.data)
+    return res.status(200).send(response.data)
 })
 
-app.post('api/order', async (req,res)=> {
-    try{
-    const response = await axios.post('http//localhost:3001/api/orders', req)
-    console.log("responceeee from back ", response.data)
-    return response.data
-    } catch (error) {
-    return { error: 'uups! Could not post order to server' }
-    }
+
+app.post('/api/orders', async (req,res)=> {
+    console.log('posting order to orderservice...')
+  try{
+    const response = await axios.post('http//order:3001/api/orders', req)
+    const body=req.body
+    console.log("oorderr responceeee from back ", body)
+    return res.status(200).send(res.json(body))
+  } catch (error) {
+     return { error: 'uups! Could not post order to server' }
+   } 
     })
 
 
@@ -39,7 +35,6 @@ app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 
 })
-
 
 
 
