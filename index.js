18b0023e73112082154dd4e1bba347bb24046c41
Virva <virva.svala@gmail.com>
2021-12-09 00:15:34 +0200

    try catch to get method

--- index.js
+++ index.js
@@ -10,33 +10,52 @@ app.use(express.static('public'))
 app.use(express.json())
 
 
-//TODO try catch
+//get orders from inventoryservise
 app.get('/api/products', async (req, res) => {
     console.log('Requesting products from nginx...')
+    try{    
     const response = await axios.get('http://nginx:9000/api/v1/products')
-    console.log("GOT responce from  inventory service ", response.data)
+    console.log("GET responce from  inventory service ", response.data)
     return res.status(200).send(response.data)
-})
+    } catch (error) {
+    if(error.response.status===500){
+    console.log("status 400  error backfront")
+    return res.status(400).sendStatus(error.response.status)
 
+    }
+    console.log("server error")
+    return res.status(500).sendStatus(error.response.status)
+    } 
 
+    })
+
+// post orders to orderservise
 app.post('/api/orders', async (req,res,)=> {
     console.log('posting order to orderservice...')
     const body=req.body
   try{
     const response = await axios.post('http://order:3001/api/orders', body)
-    console.log("responce from posting to orderservise ",response.data)
+    console.log("POST response from posting to orderservise ",response.data)
     console.log(response.status)
     if(response.status===200){
-        console.log("status 200")
+       console.log("status 200")
         console.log("status 200 ja data", response.data )
     return res.status(200).send(res.json(response.data))
-    }
+   }
 
   } catch (error) {
     console.log("catch response.status from order",error.response.status)
-     return res.status(400).sendStatus(error.response.status)
-     // return res.status(400).send(error.response.status)
-     //TO DO.. to get anwser from orderservise untill front??
+    if(error.response.status===400){
+        console.log("status 400 backfront")
+        return res.status(400).sendStatus(error.response.status)
+    }
+    if(error.response.status===500){
+        console.log("status 500 backfront")
+     return res.status(500).sendStatus(error.response.status)
+
+    }
+    console.log("unkown error")
+    return res.status(500).sendStatus(error.response.status)
    } 
     })
 
